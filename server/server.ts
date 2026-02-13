d154bda0891e84b8ebfe4283b299af9518151f2a
ShreyaJha14 <meeh.shreya.jha@gmail.com>
2026-02-14 00:39:49 +0530

    user controllers

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import "dotenv/config";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerk.js';
 import * as Sentry from "@sentry/node"
+import userRouter from "./routes/userRoutes.js";
 
 
 const app = express();
@@ -23,6 +24,11 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+app.use('/api/user', userRouter)
 
 // The error handler must be registered before any other error middleware and after all controllers
 Sentry.setupExpressErrorHandler(app);
